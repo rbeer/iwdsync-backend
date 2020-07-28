8f707b2809ddd22bbb9d1a4077c8e668faeedc17
Brian Perrett <perrettbrian@gmail.com>
2020-07-28 02:38:54 -0600

    force csrf exemption

--- caster/urlsapi.py
+++ caster/urlsapi.py
@@ -1,11 +1,12 @@
 """caster/urlsapi.py
 """
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from caster import viewsapi
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path("", viewsapi.caster),
+    path("", csrf_exempt(viewsapi.caster)),
     path("get-my-caster/", viewsapi.get_my_caster),
     path("get-csrf/", viewsapi.get_csrf),
 ]
