93c6224750227eb6c599f01bde3077dba6285413
Brian Perrett <perrettbrian@gmail.com>
2020-07-30 15:59:36 -0600

    only set samesite=None for production

--- caster/samesite.py
+++ caster/samesite.py
@@ -1,13 +1,12 @@
 from django.utils.deprecation import MiddlewareMixin
-# from django.conf import settings
 
 
 class SameSiteMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        # origin = request.headers.get('Origin')
-        # if origin in settings.CORS_ORIGIN_WHITELIST:
-        if "sessionid" in response.cookies:
-            response.cookies["sessionid"]["samesite"] = "None"
-        if "csrftoken" in response.cookies:
-            response.cookies["csrftoken"]["samesite"] = "None"
+        origin = request.headers.get('Origin', 'localhost')
+        if 'localhost' not in origin:
+            if "sessionid" in response.cookies:
+                response.cookies["sessionid"]["samesite"] = "None"
+            if "csrftoken" in response.cookies:
+                response.cookies["csrftoken"]["samesite"] = "None"
         return response
