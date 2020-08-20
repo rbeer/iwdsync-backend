ee62f8fa0a61cbcbe85b088d2a2c72e7300dcbd8
Raphael Beer <raphael.beer@protonmail.com>
2020-08-20 09:00:36 +0200

    Refactor: individual Consumers for viewers and caster

--- ws/consumers.py
+++ ws/consumers.py
@@ -14,15 +14,32 @@ class ViewerConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.caster = self.scope['url_route']['kwargs']['caster']
         await self.channel_layer.group_add(self.caster, self.channel_name)
-        await self.accept();
+        await self.accept()
 
     async def disconnect(self, close_code):
         self.channel_layer.group_discard(self.caster, self.channel_name)
 
+    async def heartbeat(self, event):
+        await self.send(text_data=json.dumps({
+            'youtube_time': event['youtube_time']
+        }))
+
+    async def control(self, event):
+        await self.send(text_data=json.dumps({
+            'control': event['control']
+        }))
+
+class CasterConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        self.user = self.scope.get('user', None)
+        if self.user.is_authenticated != True:
+            await send_error('Not authenticated.', self.send)
+            return await self.close()
+
     async def receive(self, text_data):
-        user = self.scope['user']
-        if user.is_authenticated != True:
-            return await send_error('Not authenticated.', self.send)
+        if self.user == None:
+            self.close()
 
         data = json.loads(text_data)
         url_path = data.get('url_path', None)
@@ -33,7 +50,7 @@ class ViewerConsumer(AsyncWebsocketConsumer):
         if time == None:
             return await send_error('time must be a number.', self.send)
 
-        query = Caster.objects.filter(user=user, url_path=url_path)
+        query = Caster.objects.filter(user=self.user, url_path=url_path)
         if await sta(query.exists)() == False:
             return await send_error('No caster found.', self.send)
 
@@ -42,17 +59,6 @@ class ViewerConsumer(AsyncWebsocketConsumer):
         await sta(caster.save)()
         await self.send(text_data=json.dumps({
             'status': 'ok'
-        }))
-
-    async def set_time(self, event):
-        await self.send(text_data=json.dumps({
-            'seek': event.get('seek', False),
-            'time': event['time']
-        }))
-
-    async def control(self, event):
-        await self.send(text_data=json.dumps({
-            'control': event['control']
         }))
 
 
--- ws/routing.py
+++ ws/routing.py
@@ -3,5 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 urlpatterns = [
-    re_path('ws/(?P<caster>\w+)/?$', consumers.ViewerConsumer)
+    re_path('ws/viewer/(?P<caster>\w+)/?$', consumers.ViewerConsumer),
+    re_path('ws/caster/(?P<caster>\w+)/?$', consumers.CasterConsumer)
 ]
