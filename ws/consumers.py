591d587b6a169adc54f18215cec8ec31df349753
Raphael Beer <raphael.beer@protonmail.com>
2020-08-21 01:07:38 +0200

    Change: load caster from db only once on connect

--- ws/consumers.py
+++ ws/consumers.py
@@ -29,9 +29,18 @@ class CasterConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
         self.user = self.scope.get('user', None)
+        self.url_path = self.scope['url_route']['kwargs']['caster']
+
         if self.user == None or self.user.is_authenticated != True:
             await send_error('Not authenticated.', self.send)
             return await self.close()
+
+        query = Caster.objects.filter(user=self.user, url_path=self.url_path)
+        if await sta(query.exists)() == False:
+            return await send_error('No caster found.', self.send)
+
+        self.caster = await sta(query.first)()
+
 
     async def receive(self, text_data):
         if self.user == None:
@@ -47,15 +56,10 @@ class CasterConsumer(AsyncWebsocketConsumer):
         if time == None:
             return await send_error('time must be a number.', self.send)
 
-        query = Caster.objects.filter(user=self.user, url_path=url_path)
-        if await sta(query.exists)() == False:
-            return await send_error('No caster found.', self.send)
+        self.caster.youtube_time = time
+        await sta(self.caster.save)()
 
-        caster = await sta(query.first)()
-        caster.youtube_time = time
-        await sta(caster.save)()
-
-        await self.channel_layer.group_send(caster.url_path, {
+        await self.channel_layer.group_send(self.caster.url_path, {
             'type': 'heartbeat',
             'youtube_time': time
         })
